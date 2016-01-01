5df9cce4cdf9eaa017364ac9192b1a0ba952f90a
gsvic <victorasgs@gmail.com>
2016-01-01 22:50:04 +0200

    test file

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
+import os.path as pth
 from workflow import Workflow
+import thunder
+from pyspark import SparkContext
 
-flow = Workflow("mouse-images").extract().visualize()
-print "EXPLAIN\n", flow.explain()
-flow.execute()
+sc = SparkContext()
+datapath = pth.join(pth.dirname(pth.realpath(thunder.__file__)), 'utils/data/mouse/images')
+
+flow1 = Workflow(datapath,sc).extract().clustering(k=5).visualize()
+flow2 = Workflow(datapath, sc).extract().visualize()
+
+print flow1.explain()
+print flow2.explain()
+
+flow1.execute()
 
 print "END"
